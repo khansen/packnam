f3c3e5334762572d3cfe5b7148725ccac3e46d2b
Kent Hansen <kentmhan@gmail.com>
2008-03-29 21:54:18 +0100

    separate core functionality into a separate function

--- packnam.c
+++ packnam.c
@@ -1,6 +1,24 @@
+/*
+    This file is part of packnam.
+
+    packnam is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    packnam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with packnam.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 static char program_version[] = "packnam 1.0";
 
@@ -9,7 +27,7 @@ static void usage()
 {
     printf(
         "Usage: packnam [--width=NUM] [--vram-address=NUM]\n"
-        "               [--output=FILE]\n"
+        "               [--output=FILE] [--verbose]\n"
         "               [--help] [--usage] [--version]\n"
         "                FILE\n");
     exit(0);
@@ -19,9 +37,12 @@ static void usage()
 static void help()
 {
     printf("Usage: packnam [OPTION...] FILE\n\n"
+           "packnam compresses a NES nametable.\n\n"
+           "Options:\n\n"
            "  --width=NUM                     Width of input is NUM tiles (32)\n"
            "  --vram-address=NUM              VRAM start address (0x2000)\n"
            "  --output=FILE                   Store encoded data in FILE\n"
+           "  --verbose                       Print statistics\n"
            "  --help                          Give this help list\n"
            "  --usage                         Give a short usage message\n"
            "  --version                       Print program version\n");
@@ -35,19 +56,161 @@ static void version()
     exit(0);
 }
 
+/*
+  Packs the given \a nametable and stores the result int \a out and
+  its size in \a out_sz.
+*/
+void pack_nametable(const unsigned char *nametable, int nametable_sz,
+                    int width, int vram_address, unsigned char **out, int *out_sz)
+{
+    int out_pos = 0;
+    int buf_sz = 0;
+    int y = 0;
+    int in_pos = 0;
+    assert(nametable);
+    assert(out);
+    assert(out_sz);
+    *out = 0;
+    while (y * width < nametable_sz) {
+	unsigned char rlech;
+	int count;
+	int count_pos;
+	int x = 0;
+	int state = 0;
+	int start_rle = 0;
+	int addr = vram_address + (y * 32);
+	while (x < width) {
+	    const unsigned char ch = nametable[in_pos];
+	    switch (state) {
+		case 0:
+                    start_rle = (x + 4 <= width)
+				&& (nametable[in_pos+1] == ch)
+				&& (nametable[in_pos+2] == ch)
+				&& (nametable[in_pos+3] == ch);
+                    /* fallthrough */
+		case 1:
+                    if (ch == 0) {
+                        /* don't encode 0s */
+                        start_rle = 0;
+                        ++in_pos;
+                        ++x;
+                        ++addr;
+                        break;
+                    }
+                    if (out_pos + 2 >= buf_sz) {
+                        buf_sz += 64;
+                        *out = (unsigned char *)realloc(*out, buf_sz);
+                    }
+                    (*out)[out_pos++] = (unsigned char)(addr >> 8);
+                    (*out)[out_pos++] = (unsigned char)(addr & 255);
+                    if (start_rle) {
+                        /* start of RLE run */
+                        count = 4;
+                        rlech = ch;
+                        in_pos += 4;
+                        x += 4;
+                        state = 2;
+                    } else {
+                        /* start of non-RLE run */
+                        count_pos = out_pos;
+                        if (out_pos + 2 >= buf_sz) {
+                            buf_sz += 64;
+                            *out = (unsigned char *)realloc(*out, buf_sz);
+                        }
+                        (*out)[out_pos++] = 0; /* count backpatched later */
+                        count = 1;
+                        (*out)[out_pos++] = ch;
+                        ++in_pos;
+                        ++x;
+                        state = 3;
+                    }
+                    break;
+
+		case 2:
+                    if (ch == rlech) {
+                        ++count;
+                        ++in_pos;
+                        ++x;
+                    }
+                    if ((ch != rlech) || (count == 0x3F)) {
+                        /* end RLE run */
+                        if (out_pos + 2 >= buf_sz) {
+                            buf_sz += 64;
+                            *out = (unsigned char *)realloc(*out, buf_sz);
+                        }
+                        (*out)[out_pos++] = (unsigned char)(0x40 | count);
+                        (*out)[out_pos++] = rlech;
+                        addr += count;
+                        state = 0;
+                    }
+                    break;
+
+		case 3:
+                    start_rle = ((x + 4 <= width)
+				 && (nametable[in_pos+1] == ch)
+				 && (nametable[in_pos+2] == ch)
+				 && (nametable[in_pos+3] == ch));
+                    if (start_rle) {
+                        /* end non-RLE run */
+                        (*out)[count_pos] = count;
+                        addr += count;
+                        state = 1;
+                    } else {
+                        if (out_pos + 1 >= buf_sz) {
+                            buf_sz += 64;
+                            *out = (unsigned char *)realloc(*out, buf_sz);
+                        }
+                        (*out)[out_pos++] = ch;
+                        ++in_pos;
+                        ++x;
+                        ++count;
+                        if (count == 0x3F) {
+                            /* end non-RLE run */
+                            (*out)[count_pos] = count;
+                            addr += count;
+                            state = 1;
+                        }
+                    }
+                    break;
+	    }
+	}
+	switch (state) {
+	    case 0:
+	    case 1:
+                break;
+
+	    case 2:
+                /* finish RLE run */
+                if (out_pos + 2 >= buf_sz) {
+                    buf_sz += 64;
+                    *out = (unsigned char *)realloc(*out, buf_sz);
+                }
+                (*out)[out_pos++] = (unsigned char)(0x40 | count);
+                (*out)[out_pos++] = rlech;
+                break;
+
+	    case 3:
+                /* finish non-RLE run */
+                (*out)[count_pos] = count;
+                break;
+	}
+	++y;
+    }
+    *out_sz = out_pos;
+}
+
 /**
  * Program entrypoint.
  */
 int main(int argc, char **argv)
 {
-    char nametable[1024];
+    unsigned char nametable[1024];
     int nametable_sz;
-    char *out = 0;
-    int out_pos;
+    unsigned char *out;
     int out_sz;
-    int buf_sz;
     int width = 32;
     int vram_address = 0x2000;
+    int verbose = 0;
     const char *input_filename = 0;
     const char *output_filename = 0;
     /* Process arguments. */
@@ -62,6 +225,8 @@ int main(int argc, char **argv)
                     vram_address = strtol(&opt[13], 0, 0);
                 } else if (!strncmp("output=", opt, 7)) {
                     output_filename = &opt[7];
+                } else if (!strcmp("verbose", opt)) {
+                    verbose = 1;
                 } else if (!strcmp("help", opt)) {
                     help();
                 } else if (!strcmp("usage", opt)) {
@@ -69,7 +234,8 @@ int main(int argc, char **argv)
                 } else if (!strcmp("version", opt)) {
                     version();
                 } else {
-                    fprintf(stderr, "unrecognized option `%s'\n", p);
+                    fprintf(stderr, "packnam: unrecognized option `%s'\n"
+			    "Try `packnam --help' or `packnam --usage' for more information.\n", p);
                     return(-1);
                 }
             } else {
@@ -79,10 +245,14 @@ int main(int argc, char **argv)
     }
 
     if (!input_filename) {
-        fprintf(stderr, "packnam: no filename given\n");
+        fprintf(stderr, "packnam: no filename given\n"
+                        "Try `packnam --help' or `packnam --usage' for more information.\n");
         return(-1);
     }
 
+    /* Read input file */
+    if (verbose)
+        fprintf(stdout, "reading `%s'\n", input_filename);
     {
         FILE *fp = fopen(input_filename, "rb");
         if (!fp) {
@@ -94,146 +264,27 @@ int main(int argc, char **argv)
         fclose(fp);
     }
 
-    {
-        int y = 0;
-        buf_sz = 0;
-        int in_pos = 0;
-        out_pos = 0;
-        while (y * width < nametable_sz) {
-            char rlech;
-            int count;
-            int count_pos;
-            int x = 0;
-            int state = 0;
-            int start_rle = 0;
-            int addr = vram_address + (y * 32);
-            while (x < width) {
-                const char ch = nametable[in_pos];
-                switch (state) {
-                    case 0:
-                    start_rle = (x + 4 <= width)
-                            && (nametable[in_pos+1] == ch)
-                            && (nametable[in_pos+2] == ch)
-                            && (nametable[in_pos+3] == ch);
-                    /* fallthrough */
-                    case 1:
-                    if (ch == 0) {
-                        /* don't encode 0s */
-                        start_rle = 0;
-                        ++in_pos;
-                        ++x;
-                        ++addr;
-                        break;
-                    }
-                    if (out_pos + 2 >= buf_sz) {
-                        buf_sz += 64;
-                        out = (char *)realloc(out, buf_sz);
-                    }
-                    out[out_pos++] = (char)(addr >> 8);
-                    out[out_pos++] = (char)(addr & 255);
-                    if (start_rle) {
-                        /* start of RLE run */
-                        count = 4;
-                        rlech = ch;
-                        in_pos += 4;
-                        x += 4;
-                        state = 2;
-                    } else {
-                        /* start of non-RLE run */
-                        count_pos = out_pos;
-                        if (out_pos + 2 >= buf_sz) {
-                            buf_sz += 64;
-                            out = (char *)realloc(out, buf_sz);
-                        }
-                        out[out_pos++] = 0; /* count backpatched later */
-                        count = 1;
-                        out[out_pos++] = ch;
-                        ++in_pos;
-                        ++x;
-                        state = 3;
-                    }
-                    break;
+    /* Compress */
+    if (verbose)
+        fprintf(stdout, "processing\n");
+    pack_nametable(nametable, nametable_sz, width, vram_address, &out, &out_sz);
 
-                    case 2:
-                    if (ch == rlech) {
-                        ++count;
-                        ++in_pos;
-                        ++x;
-                    }
-                    if ((ch != rlech) || (count == 0x3F)) {
-                        /* end RLE run */
-                        if (out_pos + 2 >= buf_sz) {
-                            buf_sz += 64;
-                            out = (char *)realloc(out, buf_sz);
-                        }
-                        out[out_pos++] = (char)(0x40 | count);
-                        out[out_pos++] = rlech;
-                        addr += count;
-                        state = 0;
-                    }
-                    break;
-
-                    case 3:
-                    start_rle = ((x + 4 <= width)
-                            && (nametable[in_pos+1] == ch)
-                            && (nametable[in_pos+2] == ch)
-                            && (nametable[in_pos+3] == ch));
-                    if (start_rle) {
-                        /* end non-RLE run */
-                        out[count_pos] = count;
-                        addr += count;
-                        state = 1;
-                    } else {
-                        if (out_pos + 1 >= buf_sz) {
-                            buf_sz += 64;
-                            out = (char *)realloc(out, buf_sz);
-                        }
-                        out[out_pos++] = ch;
-                        ++in_pos;
-                        ++x;
-                        ++count;
-                        if (count == 0x3F) {
-                            /* end non-RLE run */
-                            out[count_pos] = count;
-                            addr += count;
-                            state = 1;
-                        }
-                    }
-                    break;
-                }
-            }
-            switch (state) {
-                case 0:
-                case 1:
-                break;
-
-                case 2:
-                /* finish RLE run */
-                if (out_pos + 2 >= buf_sz) {
-                    buf_sz += 64;
-                    out = (char *)realloc(out, buf_sz);
-                }
-                out[out_pos++] = (char)(0x40 | count);
-                out[out_pos++] = rlech;
-                break;
-
-                case 3:
-                /* finish non-RLE run */
-                out[count_pos] = count;
-                break;
-            }
-            ++y;
-        }
-        out_sz = out_pos;
-    }
-
+    /* Write output */
     if (!output_filename)
         output_filename = "packnam.dat";
+    if (verbose)
+        fprintf(stdout, "writing data to `%s'\n", output_filename);
     {
         FILE *fp = fopen(output_filename, "wb");
         fwrite(out, 1, out_sz, fp);
         fclose(fp);
     }
 
+    if (verbose)
+        fprintf(stdout, "compressed size: %d%%\n", (out_sz*100) / nametable_sz);
+
+    free(out);
+
+    /* Success */
     return 0;
 }
